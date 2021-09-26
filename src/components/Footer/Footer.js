5398853331ead62860b6892ee6608c69588d95de
Lys <lysprestes@gmail.com>
2021-09-25 22:08:25 -0300

    Update Project

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/lysprestes'
+      className='link footer__link'
+    >
+      Made with ♡ by Lys
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,61 @@
+const header = {
+  // homepage: 'https://rajshekhar26.github.io/cleanfolio',
+  // title: 'JS.',
+}
+
+const about = {
+  name: 'Lys Prestes',
+  role: 'Front End Developer',
+  description:
+    'I am a Front End Developer with a passion for building beautiful, responsive, and interactive user interfaces. I have a strong background in both front end and back end development, and I am always looking to learn new technologies and techniques.',
+  resume: 'https://example.com',
+  social: {
+    linkedin: 'https://linkedin.com/lysprestes',
+    github: 'https://github.com/lysprestes',
+  },
+}
+
+const projects = [
+  {
+    name: 'Project 1',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 2',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 3',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+]
+
+const skills = [
+  'Git',
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Redux',
+  'Jest',
+  'Bootstrap',
+]
+
+const contact = {
+  email: 'lysprestes@mail.com',
+}
+
+export { header, about, projects, skills, contact }
